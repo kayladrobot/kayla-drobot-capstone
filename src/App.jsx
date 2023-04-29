3d694efb195fff37d09edefd47cdb49093ceb09f
Kayla Drobot <kayladrobot@gmail.com>
2023-04-28 23:22:21 -0700

    feat: set up partials

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage/Homepage"
 import Navigation from "./components/Navigation/Navigation"
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
