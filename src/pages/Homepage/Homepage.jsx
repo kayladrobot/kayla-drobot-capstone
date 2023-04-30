94db6a5aa2a63b72f9cef1aba7b740a9ba3a1f13
Kayla Drobot <kayladrobot@gmail.com>
2023-04-30 00:56:13 -0700

    feat: set up basic scss styles

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -4,15 +4,15 @@ import "./Homepage.scss";
 function Homepage() {
   return (
     <main className="home">
-      <section className="home__hero">
-        <h1>Discover 📓 Your Perfect 👌 Creative Match💘 on LookBook: </h1>
+      <section className="home__section home__hero">
+        <h1 className="home__hero-header">Discover 📓 Your Perfect 👌 Creative Match💘 on LookBook: </h1>
         <p>
           The Ultimate Creative Platform for Connection, Opportunities, and
           Collaboration
         </p>
         <button className="home__hero-button">Make A Match</button>
       </section>
-      <section className="home__creative">
+      <section className="home__section home__creative">
         <p className="p--large">Creatives</p>
         <h2>Discover Vancouver Creatives</h2>
         <div className="home__creative-cards">
@@ -23,17 +23,18 @@ function Homepage() {
         </div>
         <button>View More Creatives</button>
       </section>
-      <section className="home__jobs">
+      <section className="home__section home__jobs">
         <p className="p--large">Creatives</p>
         <h2>Discover Vancouver Creatives</h2>
-        <div className="home__creative-cards">
-          <div className="home__creative-card"></div>
-          <div className="home__creative-card"></div>
-          <div className="home__creative-card"></div>
-          <div className="home__creative-card"></div>
+        <div className="home__jobs-cards">
+          <div className="home__jobs-card"></div>
+          <div className="home__jobs-card"></div>
+          <div className="home__jobs-card"></div>
+          <div className="home__jobs-card"></div>
         </div>
+        <button>View More Jobs</button>
       </section>
-      <section className="home__project">
+      <section className="home__section home__project">
         <p className="p--large">Creatives</p>
         <h2>Discover Vancouver Creatives</h2>
         <div className="home__project-cards">
@@ -43,9 +44,10 @@ function Homepage() {
           <div className="home__project-card"></div>
           <div className="home__project-card"></div>
           </div>
+          <button>View More Projects</button>
         </div>
       </section>
-      <section className="cta">
+      <section className="home__section home__cta">
         <p className="p--large">Lorem ipsum, dolor sit amet isicing.</p>
         <h2>Welcome to your new digital reality that which will rock your world truly at all.</h2>
       <div className="cta__btn-wrapper">
