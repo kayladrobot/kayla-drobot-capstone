cb2ef0e678cf6651ab0066e1ae4fac7f6a853706
Kayla Drobot <kayladrobot@gmail.com>
2023-04-30 18:34:43 -0700

    feat: set up styling for homepage

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -5,55 +5,70 @@ function Homepage() {
   return (
     <main className="home">
       <section className="home__section home__hero">
-        <h1 className="home__hero-header">Discover 📓 Your Perfect 👌 Creative Match💘 on LookBook: </h1>
-        <p>
-          The Ultimate Creative Platform for Connection, Opportunities, and
-          Collaboration
-        </p>
-        <button className="home__hero-button">Make A Match</button>
+        <div className="home__copy-container">
+          <h1 className="home__copy-header">
+            Discover 📓 Your Perfect 👌 Creative Match💘 on LookBook:{" "}
+          </h1>
+          <p>
+            The Ultimate Creative Platform for Connection, Opportunities, and
+            Collaboration
+          </p>
+        </div>
+        <button className="home__btn--primary">Make A Match</button>
       </section>
       <section className="home__section home__creative">
-        <p className="p--large">Creatives</p>
-        <h2>Discover Vancouver Creatives</h2>
-        <div className="home__creative-cards">
+        <div className="home__copy-container">
+          <p className="p--large">Creatives</p>
+          <h2>Discover Vancouver Creatives</h2>
+        </div>
+        <div className="home__cards">
           <div className="home__creative-card"></div>
           <div className="home__creative-card"></div>
           <div className="home__creative-card"></div>
           <div className="home__creative-card"></div>
         </div>
-        <button>View More Creatives</button>
+        <button className="home__btn--primary">View More Creatives</button>
       </section>
       <section className="home__section home__jobs">
-        <p className="p--large">Creatives</p>
-        <h2>Discover Vancouver Creatives</h2>
-        <div className="home__jobs-cards">
+        <div className="home__copy-container">
+          <p className="p--large">Creatives</p>
+          <h2>Discover Vancouver Creatives</h2>
+        </div>
+        <div className="home__cards">
           <div className="home__jobs-card"></div>
           <div className="home__jobs-card"></div>
           <div className="home__jobs-card"></div>
           <div className="home__jobs-card"></div>
         </div>
-        <button>View More Jobs</button>
+        <button className="home__btn--primary">View More Jobs</button>
       </section>
       <section className="home__section home__project">
-        <p className="p--large">Creatives</p>
-        <h2>Discover Vancouver Creatives</h2>
-        <div className="home__project-cards">
+        <div className="home__copy-container">
+          <p className="p--large">Projects</p>
+          <h2>Discover Vancouver Creatives</h2>
+        </div>
+        <div className="home__cards">
           <div className="home__project-card"></div>
           <div className="home__project-card-container">
-          <div className="home__project-card"></div>
-          <div className="home__project-card"></div>
-          <div className="home__project-card"></div>
+            <div className="home__project-card"></div>
+            <div className="home__project-card"></div>
+            <div className="home__project-card"></div>
           </div>
-          <button>View More Projects</button>
+          <button className="home__btn--primary">View More Projects</button>
         </div>
       </section>
       <section className="home__section home__cta">
+      <div className="home__copy-container">
         <p className="p--large">Lorem ipsum, dolor sit amet isicing.</p>
-        <h2>Welcome to your new digital reality that which will rock your world truly at all.</h2>
-      <div className="cta__btn-wrapper">
-        <button>Take The Quiz</button>
-        <button>Make a Match</button>
-      </div>
+        <h3 className="home__copy-header">
+          Welcome to your new digital reality that which will rock your world
+          truly at all.
+        </h3>
+        </div>
+        <div className="home__btn-wrapper">
+          <button className="home__btn--secondary">Take The Quiz</button>
+          <button className="home__btn--primary">Make a Match</button>
+        </div>
       </section>
     </main>
   );
